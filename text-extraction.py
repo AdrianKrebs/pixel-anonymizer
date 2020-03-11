c3e6abeba0b49e420c7dc3008fd017d03b874334
AdrianKrebs <kra@panter.ch>
2020-03-11 09:26:10 +0100

    paramterize for second test image

--- text-extraction.py
+++ text-extraction.py
@@ -2,12 +2,12 @@ import cv2
 import numpy as np
 
 # Load image, create mask, grayscale, Gaussian blur, Otsu's threshold
-image = cv2.imread('test2_1.png')
+image = cv2.imread('test3.png')
 original = image.copy()
 blank = np.zeros(image.shape[:2], dtype=np.uint8)
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 blur = cv2.GaussianBlur(gray, (5, 5), 0)
-thresh = cv2.threshold(gray, 245, 255, cv2.THRESH_BINARY)[
+thresh = cv2.threshold(gray, 225, 255, cv2.THRESH_BINARY)[
     1]  # thresholding the image at a high intensity (since your text appears always to be white)
 
 # Merge text into a single contour
@@ -18,21 +18,13 @@ close = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel, iterations=3)  # do a 
 cnts = cv2.findContours(close, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 cnts = cnts[0] if len(cnts) == 2 else cnts[1]
 
-# a = np.array([[1, 14], [2, 15], [3, 30]])
-# b = np.array([[2, 12], [3, 12], [4, 14]])
-# c = np.array([[2, 12], [3, 12], [4, 1123]])
-#
-# test = np.array((a, b , c))
-# print(test)
-
-new_img = image
 boxList = []
 for c in cnts:
     # Filter using contour area and aspect ratio
     x, y, w, h = cv2.boundingRect(c)
     area = cv2.contourArea(c)
     ar = w / float(h)
-    if (area < 1000 and area > 10 and h < 200):  # filter areas -> what matches the patter of the text?
+    if (area < 2000 and area > 50 and h < 400):  # filter areas -> what matches the patter of the text?
         # Find rotated bounding box
         rect = cv2.minAreaRect(c)
         box = cv2.boxPoints(rect)
@@ -49,9 +41,8 @@ extract = cv2.bitwise_and(original, original, mask=extract)
 
 print(boxList)
 
-# cv2.imshow('thresh', thresh)
-# cv2.imshow('image', image)
-# cv2.imshow('close', close)
-# cv2.imshow('extract', extract)
-cv2.imshow('final', new_img)
+cv2.imshow('thresh', thresh)
+cv2.imshow('image', image)
+cv2.imshow('close', close)
+cv2.imshow('extract', extract)
 cv2.waitKey()
